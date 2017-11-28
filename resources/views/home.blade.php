fc4c7d44ee42afd8ae7c337cb7d17213fe518435
jkluio132465 <jkluio132465@gmail.com>
2017-11-28 20:48:29 +0800

    會員主頁介面

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
-
+@section('title','個人頁面')
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">過去的文章</div>
 
                 <div class="panel-body">
                     @if (session('status'))
