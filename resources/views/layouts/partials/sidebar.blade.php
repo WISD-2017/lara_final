419cab0926a20a5b361ef4737826a1e4372ba388
jkluio132465 <jkluio132465@gmail.com>
2017-11-30 02:34:04 +0800

    sidebar區塊樣板

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,68 +1,21 @@
-    <nav class="navbar navbar-Amelia navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+@include('layouts.partials.membernavbar')
+    <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        Talk
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">登入</a></li>
-                            <li><a href="{{ route('register') }}">註冊</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('home') }}">
-                                            主頁
-                                        </a>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            登出
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-            <ul class="nav navbar-nav side-nav">
-                <li class="active">
-                    <a href="#"><i class="fa fa-fw fa-dashboard"></i>全部</a>
-                </li>
-                <li>
-                    <a href="#"><i class="fa fa-fw fa-edit"></i>熱門</a>
-                </li>
-            </ul>
+    
+    <div class="sidebar">
+       <div class-sm="1"> 
+       <ul class="nav navbar-nav side-nav">
+            <li class="active">
+                <a href=""> 主控台</a>
+            </li>
+            <li>
+                <a href=""> 文章管理</a>
+            </li>
+            <li>
+                <a href="#">熱門</a>
+            </li>
+        </ul>
         </div>
-    </nav>
+    </div>
+    <!-- /.navbar-collapse -->
+</nav>
