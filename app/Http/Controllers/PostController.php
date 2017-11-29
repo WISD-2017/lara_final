aa06c19ff0a5b9e13b8135705cdb33071f54ca2a
jkluio132465 <jkluio132465@gmail.com>
2017-11-30 02:36:10 +0800

    建立Posts資料表/抓取資料

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Post;
 class PostController extends Controller
 {
    public function newest()
    {
-       return view('vi');
+    $posts = Post::orderBy('created_at','DESC')->get();
+    $data =['posts'=> $posts];
+    return view('vi',$data);
    }
    
     
