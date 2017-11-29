b11f5fda041270855a9d9a5146cb768a1fc11a47
jkluio132465 <jkluio132465@gmail.com>
2017-11-30 02:35:03 +0800

    顯示最新文章

--- resources/views/vi.blade.php
+++ resources/views/vi.blade.php
@@ -1,15 +1,37 @@
 @extends('layouts.vmi')
-@section('title','最新文章')
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">最新文章</div>
 
-                </div>
-            </div>
-        </div>
-    </div>
+@section('title', '最新文章')
+
+@section('content')
+
+
+   
+        
+   
+
+<!-- /.row -->
+<div class="container">
+    <div class="panel-heading" style="text-align:left">最新文章</div> 
+
+<div class="row">
+    <div class="col-lg-8">
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover">
+               
+               @foreach($posts as $post)
+                    <div>
+                        <div style="padding:8px">
+                        <div style="text-align:left">{{$post -> owner }}</div>
+                        <div>{{$post -> title}}</div>
+                        <div>{{$post ->updated_at}}</div>
+                        </div>
+                    </div>
+                @endforeach
+                </tbody>
+            </table>
+        </div> 
+    <div> 
 </div>
+</div>
+<!-- /.row -->
 @endsection
