9ff2719007fcb4c8c06c5d752988b4abd7143529
jkluio132465 <jkluio132465@gmail.com>
2017-11-30 02:33:20 +0800

    貼文主樣板

--- resources/views/layouts/vmi.blade.php
+++ resources/views/layouts/vmi.blade.php
@@ -1,38 +1,42 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="en">
+
 <head>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title') | Talk</title>
 
-    <title>@yield('title')|Talk</title>
-
-    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/postsi.css') }}" rel="stylesheet">
+ 
 </head>
+
 <body>
-<div id="wrapper">
 
+
+
+<div class="wrapper">
+    
         @include('layouts.partials.sidebar')
-
-        <div id="page-wrapper">
-
-            <div class="container-fluid">
-
-                @yield('content')
-
-            </div>
-            <!-- /.container-fluid -->
-
-        </div>
-        <!-- /#page-wrapper -->
-
-</div>
   
-    <!-- Scripts -->
+    
+    <div class="page-wrapper">
+            @yield('content')
+    </div>
+
+<!-- /#wrapper -->
+</div>
+
+    <!-- jQuery -->
     <script src="{{ asset('js/app.js') }}"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+  
 </body>
+
 </html>
