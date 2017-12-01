0bab31e1ee3e3fd7602fcae8d4e020a2d7e903a3
jkluio132465 <jkluio132465@gmail.com>
2017-12-02 03:21:35 +0800

    更改檔名

--- resources/views/newest.blade.php
+++ resources/views/newest.blade.php
@@ -6,8 +6,18 @@
 
 <!-- /.row -->
 <div class="container">
-    <div class="panel-heading" style="text-align:left">最新文章</div> 
+   
+    <div class="panel-heading" style="text-align:left">最新文章
+    @guest
+        <p clss="fontsmall">登入後才可發文</p>
+    @else
+        <a href="{{ route('posts.create') }}" align="right" class="btn btn-su">發文</a>   
+    @endguest                              
+    </div>
+</div> 
 
+    
+<hr  size=10 color=#888888>
 <div class="row">
     <div class="col-lg-8">
         <div class="table-responsive">
@@ -15,15 +25,16 @@
                
                @foreach($posts as $post)
                     <div>
-                    <hr width=95% size=10 color=#33ffcc>
+                    <hr  size=10 color=#888888>
                     <a href="{{ route('posts.view', $post->id) }}">
                         <div style="padding:8px">
-                        <div style="text-align:left">{{$post -> owner }}</div>
+                        <div style="text-align:left">{{$post->owner}}</div>
                         <div class="fontlarge">{{$post -> title}}</div>
                         <div>{{$post ->updated_at}}</div>
                         </div>
                         </a>
-                    </div>
+                    
+                    </div><hr size=8 color=#888888>
                 @endforeach
                 </tbody>
             </table>
