dc3972bf50ac6fe4d9e9728fbe1f9a26548623af
jkluio132465 <jkluio132465@gmail.com>
2017-12-02 08:24:57 +0800

    文章類別控制器/model/migration

---
+++ app/Http/Controllers/ClassfiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Classfi;
+use Illuminate\Http\Request;
+
+class ClassfiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Classfi  $classfi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Classfi $classfi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Classfi  $classfi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Classfi $classfi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Classfi  $classfi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Classfi $classfi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Classfi  $classfi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Classfi $classfi)
+    {
+        //
+    }
+}
---
+++ database/migrations/2017_11_30_132452_create_classfis_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClassfisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classfis', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('class')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classfis');
+    }
+}
