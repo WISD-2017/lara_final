16d129751eddca548ac60ec64518457c90e9fa44
jkluio132465 <jkluio132465@gmail.com>
2017-12-02 11:04:04 +0800

    新增類別介面

---
+++ resources/views/admin/classadd.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.memberi')
+@section('title','新增類別')
+@section('content')
+@include('layouts.partials.validation')
+
+<div class="rowclass">
+        <form action="/backstage/classfi" method = "POST" role="form">
+            {{csrf_field()}}
+            <div class="form-group">
+                <label>類別：</label>
+                <input name="class" class="form-control" placeholder="請輸入類別名稱">
+            </div>
+            <div class="text-right">
+                <button type="submit" class="btnc btn-su">新增</button>
+            </div>
+        </form>
+</div>
+@endsection
