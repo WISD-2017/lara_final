719d7d3218a2cc8bc79baae33fbd9e0553992517
jkluio132465 <jkluio132465@gmail.com>
2017-12-02 11:04:24 +0800

    管理類別介面

---
+++ resources/views/admin/classfi.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.memberi')
+@section('title','類別管理')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">文章類別
+                     <a href="{{route('classfi.add')}}" align="right" class="btnadd btn-su">新增</a>  
+                </div>
+                    
+                    @foreach($classfis as $classfi)
+                    <div class="fontbig">{{$classfi->id }} / {{$classfi->class}}
+                        <a href="#" align="right" class="btncla btn-su">編輯</a>  
+                        <a href="#" align="right" class="btncla btn-su">刪除</a>  
+                    </div>
+                    @endforeach
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
