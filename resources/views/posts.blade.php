0f878c7bfb6204f43e7ec875cc710e7af39aa00a
jkluio132465 <jkluio132465@gmail.com>
2018-01-03 04:40:03 +0800

    修改顯示文章view

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,19 +1,18 @@
 @extends('layouts.vmi')
-
-@section('title', '最新文章')
+@section('title', $post->title)
 
 @section('content')
 <!-- /.row -->
 <div class="container">
-    <div class="panel-heading,bold" style="font-weight:bold">最新文章</div> 
+    <div class="panel-heading,bold" style="font-weight:bold"></div> 
 
 <div class="row">
     <div class="col-lg-12">
 
-            @if($post != null)
+            
+           
             <div class="fontlarge" >
-                {{$post->user_name}}
-                
+                {{$post->user_name}} Talk:
             </div>
             
             <span>{{$post->updated_at}}</span>
@@ -21,7 +20,7 @@
              
              
             <div class="fontlarge" >
-               {{$post->title}}
+               標題 : {{$post->title}}
             </div>
             
             <div class="fontbig">
@@ -29,9 +28,9 @@
             </div>
             <div>&nbsp;</div>
             <div class="text-left">
-             @guest
+                @guest
                 <p clss="fontsmall">登入後才可留言</p>
-            @else
+                @else
                 <a href="{{ route('posts.comment',$post->id) }}" align="right" class="btn btn-su">留言</a> 
             </div>
             @endguest
@@ -39,21 +38,14 @@
                 <div>
                         <hr  size=10 color=#888888 width=800px align=left>
                             <div style="padding:8px">
-                            <div style="text-align:left">{{$comment -> owner }}</div>
+                            <div style="text-align:left">{{$comment -> owner }}Talk:</div>
                             
                             <div> {{$comment ->updated_at}}</div>
-                            <div class="fontlarge">{{$comment -> title}}</div>
                             <div class="fontlarge">{{$comment->content}}</div>
                             </div>
                         </div>
                         <hr  size=10 color=#888888>
             @endforeach
-            
-            @else
-            </p>
-                找不到這篇文章喔
-            </p>
-            @endif
     </div>
 </div>
 <!-- /.row -->
