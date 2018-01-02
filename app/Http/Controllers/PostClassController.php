4e5c5427885250afc5b7bee46e697895c56abf9b
jkluio132465 <jkluio132465@gmail.com>
2018-01-03 03:48:53 +0800

    文章分類列表

---
+++ app/Http/Controllers/PostClassController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+use App\Classfi;
+use DB;
+class PostClassController extends Controller
+{
+    //顯示文章類別
+    public function show()
+    {   
+        $classfis = DB::table('classfis')->get();
+        return view('classview',['classfis'=>$classfis]);
+    }
+    public function showpost($id)
+    {   
+        $posts = Post::where('classfi_id',$id)->orderBy('created_at','DESC')->get();
+        
+        return view('classposts',['posts'=>$posts]);
+    }
+}
