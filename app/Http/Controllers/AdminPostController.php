761386c2511f9471c687f92a9780b291c2d79b29
jkluio132465 <jkluio132465@gmail.com>
2018-01-09 19:55:36 +0800

    新增後臺文章刪除控制器

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Middleware\CheckAdmin;
 use Auth;
-use App\Classfi;
 use DB;
+use App\Classfi;
+use App\Comment;
+use App\Post;
 class AdminPostController extends Controller
 {
     public function __construct()
@@ -17,14 +19,34 @@ class AdminPostController extends Controller
     public function index()
     {
         $admin = Auth::user()->admin;
-        if($admin == FALSE)
-        {
-            return redirect('/');   
-        }
-        else
         return view('admin.index');
     }
 
+
+    public function posts()
+    {
+        $post = Post::orderBy('created_at','DESC')->get();
+        return view('admin.posts',['post'=> $post]);
+    }
+
+    public function postcheck($id)
+    {
+        $post=Post::find($id);
+        return view('admin.postdelet',['post'=>$post]);
+    }
+
+    public function postdelete($id)
+    {   
     
+        $comment=DB::table('comments')->where('post_id','=',$id)->get();
+        
+        if($comment!=null){
+            foreach($comment as $comment)
+            DB::table('comments')->where('post_id', '=', $comment->post_id)->delete();
+            
+        }
+        Post::destroy($id);
+        return redirect('admin/posts');
+    }
 }
 
