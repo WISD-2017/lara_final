e74683c08b7b043d7b3fa5cded3f3faea0db944a
jkluio132465 <jkluio132465@gmail.com>
2018-01-09 19:06:03 +0800

    使用者名稱編輯控制器

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Auth;
+use App\Http\Requests\MemberRequest;
+use Auth; 
 use DB;
 use App\Post;
 use App\User;
@@ -21,5 +22,26 @@ class HomeController extends Controller
         $id =  Auth::user()->id;
         $posts = DB::table('posts')->where('user_id','=' ,$id)->get();        
         return view ('home',['posts'=>$posts]);
+    }
+
+    public function member()
+    { 
+        $id =  Auth::user()->id;
+        $member = DB::table('users')->where('id','=',$id)->get();        
+        return view ('memberdata',['member'=>$member]);
+    }
+    
+    public function memberedit()
+    { 
+        $id =  Auth::user()->id;
+        $member = DB::table('users')->where('id','=',$id)->get();        
+        return view ('memberedit',['member'=>$member]);
+    }
+    
+    public function memberupdate(MemberRequest $request,$id)
+    {
+        $member = User::find($id);
+        $member->update($request->all());
+        return redirect()->route('home');  
     }
 }
